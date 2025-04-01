2fad0cde942edea92c658da186231ae1d932e25a
anandhu <anandhunair777@gmail.com>
2025-04-01 14:51:09 +0530

    width issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import TaskSection from "./component/TaskSection";
 function App() {
   return (
     <>
-      <div >
+      <div className="flex justify-center">
+        <div className="max-w-[1200px] ">
         <div >
           <HeroSection></HeroSection>
         </div>
         <div className="mb-[10%]">
           <TaskSection></TaskSection>
         </div>
+        </div>
+      
       </div>
     </>
   );
